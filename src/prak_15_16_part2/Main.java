7a47227e1f457d62541cd447f6fb10bc6fc509cc
Ann_Savitskay <ansavitskayloki@gmail.com>
2021-09-28 12:31:09 +0300

    prak_15_16_part2 1-st version

---
+++ src/prak_15_16_part2/Main.java
@@ -0,0 +1,9 @@
+package prak_15_16_part2;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Terminal terminal = new Terminal();
+        terminal.start_work();
+    }
+}
