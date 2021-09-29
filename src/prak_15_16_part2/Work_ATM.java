56a8eb17b324c70287665cc47a7489c65a14be5b
Ann_Savitskay <ansavitskayloki@gmail.com>
2021-09-29 16:35:16 +0300

    prak_15_16_part2 3-rd version

--- src/prak_15_16_part2/Work_ATM.java
+++ src/prak_15_16_part2/Work_ATM.java
@@ -16,35 +16,6 @@ public class Work_ATM implements Runnable{
     public void run()
     {
         this.work();
-/*        while (true)
-        {
-            synchronized (person)
-            {
-                try {
-                    this.Add();
-                    this.Subtract();
-                    if(person.getBalance() < 0) throw new Exception();
-                    System.out.println(Thread.currentThread().getName() + " " + person.toString());
-//                    Terminal.monitor.notify();
-
-//                    Terminal.monitor.wait();
-                    Thread.sleep(sleep);
-                }
-                catch (InterruptedException e)
-                {
-                    e.printStackTrace();
-                    Thread.currentThread().interrupt();
-                    break;
-                }
-                catch (Exception e) {
-                    System.out.println("Balance is not enough");
-                    Thread.currentThread().interrupt();
-                    break;
-                }
-            }
-        }
-
- */
     }
 
     public synchronized void work()
