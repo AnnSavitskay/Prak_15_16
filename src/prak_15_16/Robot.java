30e9564787cd01862976b9b8b741690f009fc119
Ann_Savitskay <ansavitskayloki@gmail.com>
2021-10-05 10:26:25 +0300

    prak_15_16_part2 1-st version

--- src/prak_15_16/Robot.java
+++ src/prak_15_16/Robot.java
@@ -1,14 +1,22 @@
 package prak_15_16;
 
 public class Robot {
-    public Step leg_left = new Step("LEFT");
-    public Step leg_right = new Step("RIGHT");
+    public Step leg_left = new Step(this,"LEFT");
+    public Step leg_left2 = new Step(this,"LEFT2");
+    public Step leg_right = new Step(this,"RIGHT");
+    public Step leg_right2 = new Step(this,"RIGHT2");
+    public Robot(){}
 
     public void stepping()
     {
         Thread step_left = new Thread(leg_left);
         Thread step_right = new Thread(leg_right);
+        Thread step_right2 = new Thread(leg_right2);
+        Thread step_left2 = new Thread(leg_left2);
         step_left.start();
         step_right.start();
+        step_left2.start();
+        step_right2.start();
     }
+
 }
--- src/prak_15_16/Step.java
+++ src/prak_15_16/Step.java
@@ -1,24 +1,27 @@
 package prak_15_16;
 
 public class Step implements Runnable{
+    protected Robot robot;
     protected String which_leg;
-    public Step(String which_leg)
+    public Step(Robot robot, String w)
     {
-        this.which_leg = which_leg;
+        this.robot = robot;
+        this.which_leg = w;
     }
 
     public void run()
     {
         while(true)
         {
-            synchronized (which_leg) {
+            synchronized (robot) {
                 System.out.println(which_leg);
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    Thread.currentThread().interrupt();
-                }
+            }
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
             }
         }
     }
